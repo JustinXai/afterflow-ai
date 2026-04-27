22fcf1755c0797f530ac839789fe1700d110d722
xdz199110-lang <xdz199110@gmail.com>
2026-04-27 11:58:46 +0800

    fix: explicit vite alias for SSR build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
   server: {
     host: "localhost",
     port: 3000,
